7cf8e019d390da72744bbb2e95a2b8a61ebcfed5
Anatolii Ocheretnyi <42618934+AnatoliiOcheretnyi@users.noreply.github.com>
2019-07-12 17:16:32 +0300

    Update formatTime.js

--- FL11_HW8/homework/js/formatTime.js
+++ FL11_HW8/homework/js/formatTime.js
@@ -5,6 +5,4 @@ function formatTime(number){
     return day + ' day(s)' + hours + ' hour(s)' + minutes + ' minute(s)';
 }
 
-console.log(formatTime(120));
-console.log(formatTime(59));
-console.log(formatTime(3601));
+formatTime(3601);
