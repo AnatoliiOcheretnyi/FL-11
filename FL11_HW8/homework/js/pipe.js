73a88b84341c4821d26bee1c20b1f9e87510964e
Anatolii Ocheretnyi <42618934+AnatoliiOcheretnyi@users.noreply.github.com>
2019-07-12 22:16:01 +0300

    Update pipe.js

--- FL11_HW8/homework/js/pipe.js
+++ FL11_HW8/homework/js/pipe.js
@@ -1,10 +1,12 @@
 function addOne(x){
-    return x +1;
+    return x + 1;
 }
 
-function pipe(number, ...arguments){
-    
+function pipe(number, ...pipeFunc){
+    for(let i = 0; i < pipeFunc.length; i++){
+        number = pipeFunc[i](number)
+    }
+    return number
 }
 
-console.log(pipe(1, addOne));
 console.log(pipe(1, addOne, addOne));
