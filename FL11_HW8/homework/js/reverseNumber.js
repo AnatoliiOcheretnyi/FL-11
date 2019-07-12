a3d3ac59a579e35a5e393e092f549d769ca73f36
Anatolii Ocheretnyi <42618934+AnatoliiOcheretnyi@users.noreply.github.com>
2019-07-12 22:49:14 +0300

    Update reverseNumber.js

--- FL11_HW8/homework/js/reverseNumber.js
+++ FL11_HW8/homework/js/reverseNumber.js
@@ -1,5 +1,17 @@
 function reverseNumber(number) {
-    
+    let result = 0;
+    let negativeNumber = number < 0;
+    number = Math.abs(number);
+    while (number) {
+      result = result * 10 + number % 10;
+      number = Math.floor(number / 10);
+    }
+    if(negativeNumber){
+        result *= -1
+    }
+    return result;
 }
+
+reverseNumber(-123);
 
 
