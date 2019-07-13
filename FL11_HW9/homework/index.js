10655063a281aa60ccb8dc29f0e0347d109012d5
Anatolii Ocheretnyi <42618934+AnatoliiOcheretnyi@users.noreply.github.com>
2019-07-13 17:21:04 +0300

    add 0 task

--- FL11_HW9/homework/index.js
+++ FL11_HW9/homework/index.js
@@ -1,6 +1,13 @@
 function getNumbers(string){
-    string = string.split('').map(Number).filter(Boolean);
-    return string
+    let numberMass = [], newMass = [];
+    for(let i = 0; i < string.length; i++){
+        newMass.push(string.substr([i], 1))
+    }
+    for(let i of newMass){
+        i = +i;
+        if(!isNaN(i)){
+            numberMass.push(i);
+        }
+    }
+    return numberMass;
 }
-
-console.log(getNumbers('n1um3ber95'));
