8cfb73a4beb9dd915c935d35a56b8b39b4a21e7e
Anatolii Ocheretnyi <42618934+AnatoliiOcheretnyi@users.noreply.github.com>
2019-07-16 01:13:24 +0300

    update getNumbers function

--- FL11_HW9/homework/index.js
+++ FL11_HW9/homework/index.js
@@ -33,14 +33,11 @@ const data = [
     }
   ]
 
-function getNumbers(string){
-    let numberMass = [], newMass = [];
-    for(let i = 0; i < string.length; i++){
-        newMass.push(string.substr([i], 1))
-    }
-    for(let i of newMass){
-        i = +i;
-        if(!isNaN(i)){
+ function getNumbers(string) {
+    let numberMass = [];
+    for (let i of string) {
+        i = +i
+        if (!isNaN(i)) {
             numberMass.push(i);
         }
     }
