cc2a799ec80eb29e574f047103ee8248e2940ba1
Anatolii Ocheretnyi <42618934+AnatoliiOcheretnyi@users.noreply.github.com>
2019-07-23 22:08:31 +0300

    Update app.js

--- FL11_HW11/homework/src/app.js
+++ FL11_HW11/homework/src/app.js
@@ -1,38 +1,32 @@
-let rootNode = document.getElementById("root");
-let addBtn = document.getElementById('add-button');
-let list = document.getElementById('task-list');
-let addAction = document.getElementById('get-text');
-let checkboxes = document.querySelectorAll('input[type="checkbox"');
-addBtn.onclick = function() {
-    if (addAction.value != "") {
-        let item = document.createElement('slot');
-        item.classList.add('tasks-list');
-        item.innerHTML = `
-        <div  class="edit-tasks hide">
-                <div class="task-list_item hide">
-                  <div class="task-list_item-check">
-                      <input type="text" placeholder="let's kill cat">
-                  </div>
-                  <i class="material-icons save">save</i>
-                </div>
-              </div>
-
-        <div class="task-list_item">
-              <div class="task-list_item-check">
-                  <input type="checkbox" class="checkbox" id="1">
-                  <label>${addAction.value}</label>
-              </div>
-              <i class="material-icons edit">edit</i>
-            </div>
+let rootNode = document.getElementById('root');
+let addButtone = document.getElementById('add-button');
+let taskList = document.getElementById('toDoList');
+let addInput = document.getElementById('get-text');
+let checkbox = document.querySelectorAll('input[type="checkbox"');
+addButtone.onclick = function() {
+    if (addInput.value !== '') {
+        let item = document.createElement('div');
+        item.classList.add('inline-item');
+        item.innerHTML = `<div class="deal hidden">
+            <input type="text" class="edition">
+            <i class="material-icons save">save</i>
+        </div>
+        <div class='deal edit'>
+        <div class='task'>
+            <input type="checkbox">
+            <li class="list-item">${addInput.value}</li>
+            <i class="material-icons edit">create</i>
+        </div>
             <i class="material-icons delete">delete</i>
-        
-   `;
-        list.appendChild(item);
-        addAction.value = "";
+        </div>`;
+        taskList.appendChild(item);
+        addInput.value = '';
     }
 }
 document.addEventListener('change', function(e) {
-    if (e.target.type === 'checkbox') e.target.disabled = true;
+    if (e.target.type === 'checkbox'){
+        e.target.disabled = true;
+    } 
 
 });
 document.addEventListener('click', function(e) {
@@ -40,16 +34,17 @@ document.addEventListener('click', function(e) {
         e.target.parentElement.remove();
     }
     if (e.target.classList.contains('edit')) {
-        if (e.target.parentElement.childNodes[0].nodeName == '#text') {
-            e.target.parentElement.childNodes[0].nextSibling.style.display = 'flex';
+        if (e.target.parentElement.parentElement.parentElement.childNodes[0].nodeName === '#text') {
+            e.target.childNodes[0].nextSibling.style.display = 'flex';
         } else {
-            e.target.parentElement.childNodes[0].style.display = 'flex';
+            e.target.parentElement.parentElement.parentElement.childNodes[0].style.display = 'flex';
         }
         e.target.parentElement.parentElement.style.display = 'none';
     }
     if (e.target.classList.contains('save')) {
         if (e.target.previousElementSibling.value !== '') {
-            e.target.parentElement.nextElementSibling.childNodes[1].childNodes[3].innerText = e.target.previousElementSibling.value;
+            e.target.parentElement.nextElementSibling.childNodes[1].childNodes[3].innerText = 
+            e.target.previousElementSibling.value;
             e.target.previousElementSibling.value = '';
             e.target.parentElement.style.display = 'none';
             e.target.parentElement.nextElementSibling.style.display = 'flex';
