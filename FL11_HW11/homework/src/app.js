3e41f4fb3a29917906b6c9723ddf65023c3863a3
Anatolii Ocheretnyi <42618934+AnatoliiOcheretnyi@users.noreply.github.com>
2019-07-23 20:51:49 +0300

    Update app.js

--- FL11_HW11/homework/src/app.js
+++ FL11_HW11/homework/src/app.js
@@ -1,3 +1,58 @@
 let rootNode = document.getElementById("root");
+let addBtn = document.getElementById('add-button');
+let list = document.getElementById('task-list');
+let addAction = document.getElementById('get-text');
+let checkboxes = document.querySelectorAll('input[type="checkbox"');
+addBtn.onclick = function() {
+    if (addAction.value != "") {
+        let item = document.createElement('slot');
+        item.classList.add('tasks-list');
+        item.innerHTML = `
+        <div  class="edit-tasks hide">
+                <div class="task-list_item hide">
+                  <div class="task-list_item-check">
+                      <input type="text" placeholder="let's kill cat">
+                  </div>
+                  <i class="material-icons save">save</i>
+                </div>
+              </div>
 
-// Your code goes here
+        <div class="task-list_item">
+              <div class="task-list_item-check">
+                  <input type="checkbox" class="checkbox" id="1">
+                  <label>${addAction.value}</label>
+              </div>
+              <i class="material-icons edit">edit</i>
+            </div>
+            <i class="material-icons delete">delete</i>
+        
+   `;
+        list.appendChild(item);
+        addAction.value = "";
+    }
+}
+document.addEventListener('change', function(e) {
+    if (e.target.type === 'checkbox') e.target.disabled = true;
+
+});
+document.addEventListener('click', function(e) {
+    if (e.target.classList.contains('delete')) {
+        e.target.parentElement.remove();
+    }
+    if (e.target.classList.contains('edit')) {
+        if (e.target.parentElement.childNodes[0].nodeName == '#text') {
+            e.target.parentElement.childNodes[0].nextSibling.style.display = 'flex';
+        } else {
+            e.target.parentElement.childNodes[0].style.display = 'flex';
+        }
+        e.target.parentElement.parentElement.style.display = 'none';
+    }
+    if (e.target.classList.contains('save')) {
+        if (e.target.previousElementSibling.value !== '') {
+            e.target.parentElement.nextElementSibling.childNodes[1].childNodes[3].innerText = e.target.previousElementSibling.value;
+            e.target.previousElementSibling.value = '';
+            e.target.parentElement.style.display = 'none';
+            e.target.parentElement.nextElementSibling.style.display = 'flex';
+        }
+    }
+});
