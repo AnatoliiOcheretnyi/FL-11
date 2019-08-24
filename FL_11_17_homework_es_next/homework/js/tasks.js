762584c37b4df3b28645b2f639992cc9cfa6338f
Anatolii <anatoliy.ocheretnyi@gmail.com>
2019-08-24 22:43:38 +0300

    3 task is added

--- FL_11_17_homework_es_next/homework/js/tasks.js
+++ FL_11_17_homework_es_next/homework/js/tasks.js
@@ -1,9 +1,9 @@
 // task 1
-let maxElement = (...arr) => Math.max(...arr);
+let maxElement = (arr) => Math.max(...arr);
 
 const array = [1, 2, 5, 1]
 
-console.log(maxElement(...array));
+console.log(maxElement(array));
 
 // task 2
 let coppyArray = (arr) => newArr = [...arr];
@@ -11,4 +11,16 @@ let copiedArray = coppyArray(array)
 console.log(copiedArray);
 console.log(array === copiedArray);
 
+// task 3
+let addUniqueId = (object) => {
+    let newObj = {...object}
+    let objID = Symbol('_id:111341871')
+    newObj.id = objID
+    return newObj
+};
 
+let myProfile = {name: 'Anatolii', age: 20};
+console.log(addUniqueId(myProfile));
+console.log(myProfile);
+
+//task 4
