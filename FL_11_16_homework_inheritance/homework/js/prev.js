88bcbc2df52ddea9a6452e9cfd381aa811086f6f
Anatolii <anatoliy.ocheretnyi@gmail.com>
2019-08-24 22:24:13 +0300

    it was mistaked commit to 16 homework

--- FL_11_16_homework_inheritance/homework/js/prev.js
+++
@@ -1,5 +0,0 @@
-Number.prototype.sum = function(num){
-    return this + num
-}
-
-console.log((5).sum(10));
--- FL_11_16_homework_inheritance/homework/js/sgor.js
+++
@@ -1,3 +0,0 @@
-console.log('running index.js');
-import { sum } from './prev.js';
-console.log(sum(1, 2));
